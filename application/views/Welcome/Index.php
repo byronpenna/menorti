05d80a1f5b9fc727b68a7e977b5f4373d6b214d0
Byron Peña <byronpenna@gmail.com>
2017-05-14 16:20:38 -0600

    Cambio de url

---
+++ application/views/Welcome/Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Titulo</title>
+	<?php
+		$this->load->view("parts/head.php");
+	?>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("content/css/paginas/Welcome/Index/")."style.css"; ?> >
+</head>
+<body>
+
+</body>
+</html>
