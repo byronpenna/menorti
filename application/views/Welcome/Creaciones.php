65e86c1b281afbe8f632df4bf76e916e05008ca2
Byron Peña <byronpenna@gmail.com>
2017-05-25 05:46:54 -0600

    pagina creaciones

--- application/views/Welcome/Creaciones.php
+++ application/views/Welcome/Creaciones.php
@@ -1,18 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Titulo</title>
+	<title>Creaciones</title>
 	<?php
 		$this->load->view("parts/head.php");
 	?>
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("content/css/paginas/Welcome/Index/")."style.css"; ?> >
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("content/css/paginas/Welcome/Creaciones/")."style.css"; ?> >
 </head>
 <body>
 	<?php 
 	$this->load->view("parts/header.php");
 	?>
 	<div class="row marginNull tarjeta">
-		
+		<div class="col-lg-2">
+			<div class="contenedorImg row marginNull">
+				<img class="portada" src=<?php echo base_url("content/img/welcome/index/portada1.jpg") ?> >
+			
+			</div>
+						
+		</div>
+		<div class="col-lg-10">
+			<div class="row marginNull cuerpoTarjeta">
+				<div class="titulo">
+					Romance de las rosa - Amor apasionado en cenizas
+				</div>
+				<div class="descripcion">
+					La encarnación de las “Lágrimas de Espíritus Celestiales” ha comenzado; 
+					nuevas criaturas que nacen a los siete días; Eternidad es su deseo y divinidad 
+					es su Obsesión. Una lágrima ha sido encarnada y su nombre es Adrián, al nacer es dividido su corazón en siete fragmentos para destinarse a siete amores 
+				</div>
+				<div class="footerTarjeta row marginNull"> 
+					<div class="col-lg-6 izquierda">
+						<img src=<?php echo base_url("content/img/general/fb-square.png") ?> >
+					</div>
+					<div class="col-lg-6 derecha">
+						<span class="price ">$19.99</span>
+					</div>
+				</div>
+			</div>
+
+		</div>
 	</div>
 	<div class="row marginNull">
 		
