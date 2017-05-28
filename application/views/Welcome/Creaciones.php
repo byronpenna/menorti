09fe69350dad58aa9e0f6ecfd8bb3d6b428d9c1a
Byron Peña <byronpenna@gmail.com>
2017-05-28 16:33:52 -0600

    Pagina creaciones

--- application/views/Welcome/Creaciones.php
+++ application/views/Welcome/Creaciones.php
@@ -31,18 +31,59 @@
 				</div>
 				<div class="footerTarjeta row marginNull"> 
 					<div class="col-lg-6 izquierda">
-						<img src=<?php echo base_url("content/img/general/fb-square.png") ?> >
+						<a href="https://www.facebook.com/ritsa.es">
+							<img class="imgSocial" src=<?php echo base_url("content/img/general/fb-square.png") ?> >
+						</a>
+						<a href="https://www.blogger.com/">
+							<img class="imgSocial" src=<?php echo base_url("content/img/general/bloguer-square.png") ?> >
+						</a>
+						<a href="twitter.com/Ritsasv_">
+							<img class="imgSocial" src=<?php echo base_url("content/img/general/twitter-square.png") ?> >
+						</a>
 					</div>
 					<div class="col-lg-6 derecha">
 						<span class="price ">$19.99</span>
 					</div>
 				</div>
 			</div>
-
 		</div>
 	</div>
-	<div class="row marginNull">
-		
+	<div class="row marginNull tarjeta">
+		<div class="col-lg-2">
+			<div class="contenedorImg row marginNull">
+				<img class="portada" src=<?php echo base_url("content/img/welcome/index/portada2.jpg") ?> >
+			
+			</div>
+						
+		</div>
+		<div class="col-lg-10">
+			<div class="row marginNull cuerpoTarjeta">
+				<div class="titulo">
+					Romance de las rosa - Amor apasionado en cenizas
+				</div>
+				<div class="descripcion">
+					La encarnación de las “Lágrimas de Espíritus Celestiales” ha comenzado; 
+					nuevas criaturas que nacen a los siete días; Eternidad es su deseo y divinidad 
+					es su Obsesión. Una lágrima ha sido encarnada y su nombre es Adrián, al nacer es dividido su corazón en siete fragmentos para destinarse a siete amores 
+				</div>
+				<div class="footerTarjeta row marginNull"> 
+					<div class="col-lg-6 izquierda">
+						<a href="https://www.facebook.com/ritsa.es">
+							<img class="imgSocial" src=<?php echo base_url("content/img/general/fb-square.png") ?> >
+						</a>
+						<a href="https://www.blogger.com/">
+							<img class="imgSocial" src=<?php echo base_url("content/img/general/bloguer-square.png") ?> >
+						</a>
+						<a href="twitter.com/Ritsasv_">
+							<img class="imgSocial" src=<?php echo base_url("content/img/general/twitter-square.png") ?> >
+						</a>
+					</div>
+					<div class="col-lg-6 derecha">
+						<span class="price ">$19.99</span>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	<?php 
 		$this->load->view("parts/footer.php");
